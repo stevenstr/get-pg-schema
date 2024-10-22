2d9e3221810875c1b2a62db9affc4d73d34c730b
SS <stephenzhavrid@gmail.com>
2024-10-22 18:41:55 +0300

    create godoc example for access to docs)

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ The utility helps to verify that you can connect to a PostgreSQL database and ge
 
 
 # Setup
+- put the following commands to cmd.
 ```sh
 git clone git@github.com:stevenstr/get-pg-schema.git
 cd ./get-pg-schema
@@ -11,11 +12,21 @@ docker-compose up
 ```
 
 # Usage
-for testing
+- for testing.
 ```sh
 go run .\get-pg-schema.go localhost 5432 stevenstr pass master
 ```
-for "real" usage
+- for "real" usage.
 ```sh
 get-pg-schema.exe localhost 5432 stevenstr pass master
+```
+
+# Acces to docs
+- puth to cmd and press enter.
+```sh
+godoc -http=:6060
+```
+- go to browser.
+```sh
+http://localhost:6060/pkg/get-pg-schema/
 ```
--- get-pg-schema.go
+++ get-pg-schema.go
@@ -1,5 +1,10 @@
+// The utility helps to verify that you can
+// connect to a PostgreSQL database and get
+// a list of available databases and tables
+// in that database and public schema.
 package main
 
+// Imports all of the needed packages.
 import (
 	"database/sql"
 	"fmt"
@@ -9,6 +14,7 @@ import (
 )
 
 func main() {
+	// Try to parse the arguments from the cmd .
 	arguments := os.Args
 
 	if len(arguments) != 6 {
@@ -16,14 +22,17 @@ func main() {
 		return
 	}
 
+	// Put the values to the vars.
 	host := arguments[1]
 	port := arguments[2]
 	usrName := arguments[3]
 	password := arguments[4]
 	dbName := arguments[5]
 
+	// Create string of connection data.
 	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, usrName, password, dbName)
 
+	// Try to open connection to db.
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
 		fmt.Println("Open():", err)
@@ -31,6 +40,7 @@ func main() {
 	}
 	defer db.Close()
 
+	// Try to read databases names.
 	rows, err := db.Query(`SELECT "datname" FROM "pg_database"
 	WHERE datistemplate = false`)
 
@@ -50,6 +60,7 @@ func main() {
 	}
 	defer rows.Close()
 
+	// Try to read tables names.
 	rows, err = db.Query(`SELECT table_name FROM information_schema.tables
 	WHERE table_schema='public'
 	ORDER BY table_name`)
@@ -59,6 +70,7 @@ func main() {
 		return
 	}
 
+	// BUG(r): Just the bug example)).
 	for rows.Next() {
 		var name string
 		err = rows.Scan(&name)
@@ -69,5 +81,4 @@ func main() {
 		fmt.Println("+T", name)
 	}
 	defer rows.Close()
-
 }
